863020132bb449ecd9873f0840b2ac06afc48344
Teja Kommineni <teja.kommineni1@gmail.com>
2017-09-03 16:12:34 -0600

    LeetCode Contest - Maximum Swap

--- Contests/Contest48.java
+++ Contests/Contest48.java
@@ -55,6 +55,57 @@ public class Contest48 {
 	            return trimBST(root.left,L,R);
 	        
 	    }
+	    
+	    public int maximumSwap(int num) {
+	        String number = String.valueOf(num);
+	        boolean flag = false;
+	        if(num<10)
+	            return num;
+	        int i=1;
+	        for(;i<number.length();i++)
+	        {
+	            if(number.charAt(i-1)<number.charAt(i))
+	            {
+	                flag = true;
+	                break;
+	            }
+	        }
+	        if(!flag)
+	            return num;
+	        
+	        int maxIndex = i;
+	        int max = number.charAt(i) - '0';
+	        for(int j=i;j<number.length();j++)
+	        {
+	            if(max<=(number.charAt(j)-'0'))
+	            { max = (number.charAt(j) - '0');
+	             maxIndex = j;
+	            }
+	        }
+	        int j=0;
+	        String s = "";
+	        for(;j<i;j++)
+	        {
+	            if((number.charAt(j)-'0')<max)
+	            {
+	                break;}
+	            s = s+number.charAt(j);
+	        }
+	        int swap = (number.charAt(j)-'0');
+	        s = s + String.valueOf(max);
+	        j++;        
+	        for(;j<number.length();j++)
+	        {
+	            if(j == maxIndex)
+	            {
+	                s =s + String.valueOf(swap);
+	            }
+	            else
+	            s = s+number.charAt(j);
+	        }
+	        
+	        return Integer.parseInt(s);
+	    }
 	
 	public static void main(String[] args) {
 		Contest48 cs = new Contest48();
